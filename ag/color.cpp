6407dc67574192dbae55bae9c487105e6640d50f
Adam Hojka <hojka@programmer.net>
2018-04-04 01:29:48 +0200

    Brace initialization.

--- ag/color.cpp
+++ ag/color.cpp
@@ -3,13 +3,13 @@
 namespace ag
 {
 	color::color(const uint32_t value):
-		r((value & 0x00ff0000) >> 16),
-		g((value & 0x0000ff00) >> 8),
-		b((value & 0x000000ff)),
-		a((value & 0xff000000) >> 24)
+		r{static_cast<uint8_t>((value & 0x00ff0000) >> 16)},
+		g{static_cast<uint8_t>((value & 0x0000ff00) >> 8)},
+		b{static_cast<uint8_t>((value & 0x000000ff))},
+		a{static_cast<uint8_t>((value & 0xff000000) >> 24)}
 	{}
 
 	color::color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a):
-		r(r), g(g), b(b), a(a)
+		r{r}, g{g}, b{b}, a{a}
 	{}
 }
