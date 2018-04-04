4e0ea7cfe08c4f16ccf4a51bb5ae24f402dd4367
Adam Hojka <hojka@programmer.net>
2018-04-04 12:36:50 +0200

    Makes one constructor explicit and adds a default value to second one.

--- ag/color.h
+++ ag/color.h
@@ -11,9 +11,9 @@ namespace ag
 		const uint8_t r, g, b, a;
 
 		/* Create a new color. */
-		explicit color(const uint32_t value);
+		explicit color(const uint32_t value = 0);
 
 		/* Create a new color. */
-		color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255);
+		explicit color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255);
 	};
 }
