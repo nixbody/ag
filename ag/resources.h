7ab866db42aacb3d4a998285aa22df0bf8a54e0a
Adam Hojka <hojka@programmer.net>
2018-04-06 16:19:22 +0200

    Adds Roboto and Material Icons fonts.

---
+++ ag/resources.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace ag::resources
+{
+	const std::string &material_icons_regular_woff();
+	const std::string &roboto_bold_woff();
+	const std::string &roboto_italic_woff();
+	const std::string &roboto_light_woff();
+	const std::string &roboto_medium_woff();
+	const std::string &roboto_regular_woff();
+}
