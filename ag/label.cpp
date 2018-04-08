8c93474357bcf372afb623086d67074bb420d0ac
Adam Hojka <hojka@programmer.net>
2018-04-08 17:23:43 +0200

    Fixes text alignment.

--- ag/label.cpp
+++ ag/label.cpp
@@ -16,6 +16,14 @@ namespace ag
 	{
 		component::draw();
 		const auto &s = style();
-		s.text_font().draw_text(text_, s.x(), s.y(), s.width(), s.text_color(), s.line_height(), s.text_align());
+		float x;
+
+		switch (s.text_align()) {
+			case font::alignment::left: x = s.x(); break;
+			case font::alignment::center: x = s.width() / 2.0f; break;
+			case font::alignment::right: x = s.width(); break;
+		}
+
+		s.text_font().draw_text(text_, x, s.y(), s.width(), s.text_color(), s.line_height(), s.text_align());
 	}
 }
