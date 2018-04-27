19ef55e3abfcca35cbc1718298c7eabc72a8a893
Adam Hojka <hojka@programmer.net>
2018-04-28 01:45:00 +0200

    Presume solid color when alpha is not specified (is zero).

--- ag/color.cpp
+++ ag/color.cpp
@@ -1,12 +1,18 @@
 #include "color.h"
 
+constexpr uint8_t alpha(const uint32_t value)
+{
+	const auto a = static_cast<uint8_t>((value & 0xff000000u) >> 24u);
+	return a ? a : 255u;
+}
+
 namespace ag
 {
 	color::color(const uint32_t value):
-		r{static_cast<uint8_t>((value & 0x00ff0000) >> 16)},
-		g{static_cast<uint8_t>((value & 0x0000ff00) >> 8)},
-		b{static_cast<uint8_t>((value & 0x000000ff))},
-		a{static_cast<uint8_t>((value & 0xff000000) >> 24)}
+		r{static_cast<uint8_t>((value & 0x00ff0000u) >> 16u)},
+		g{static_cast<uint8_t>((value & 0x0000ff00u) >> 8u)},
+		b{static_cast<uint8_t>((value & 0x000000ffu))},
+		a{alpha(value)}
 	{}
 
 	color::color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a):
