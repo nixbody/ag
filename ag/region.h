22f8ebceaf6f82181e01c3317847539562393370
Adam Hojka <hojka@programmer.net>
2018-05-16 22:37:54 +0200

    Use a delegating constructor instead.

--- ag/region.h
+++ ag/region.h
@@ -11,11 +11,11 @@ namespace ag
 		const float x, y, width, height;
 
 		/* Construct a new region based on the given coordinates and size. */
-		region(float x, float y, float width, float height): x{x}, y{y}, width{width}, height{height}
+		constexpr region(float x, float y, float width, float height) noexcept: x{x}, y{y}, width{width}, height{height}
 		{}
 
 		/* Construct a new region based on the given component. */
-		region(const component &c): x{c.x()}, y{c.y()}, width{c.width()}, height{c.height()}
+		region(const component &c): region(c.x(), c.y(), c.width(), c.height())
 		{}
 
 		/* Tell whether or not this region contains the given coordinates. */
