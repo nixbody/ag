81100e961dcc03cfd51828d7e4c26af914032dbe
Adam Hojka <hojka@programmer.net>
2018-06-08 19:14:25 +0200

    Adds semi-transparent border to buttons.

--- ag/button.h
+++ ag/button.h
@@ -15,9 +15,10 @@ namespace ag
 		/* Initialize a new button. */
 		button()
 		{
-			width = [this] { return font().text_width(text()) + 31.0f; };
-			height = [this] { return font().line_height() + 19.0f; };
+			width = [this] { return font().text_width(text()) + padding().left + padding().right + 2.0f * border().thickness; };
+			height = [this] { return font().line_height() + padding().top + padding().bottom + 2.0f * border().thickness; };
 			padding = [] { return get_theme().button_padding(); };
+			border = {1.0f, color{0, 0, 0, 30}};
 			radius = [] { return get_theme().button_radius(); };
 			bg_color = [] { return get_theme().button_primary_color(); };
 			font = [] { return get_theme().button_font(); };
