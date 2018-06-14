1a173585c9c192e9bd03981dd50045832ef340c2
Adam Hojka <hojka@programmer.net>
2018-06-14 13:36:29 +0200

    Different button types.

--- ag/button.h
+++ ag/button.h
@@ -10,8 +10,14 @@ namespace ag
 	class button : public component
 	{
 	public:
+		/* Button type. */
+		enum class type {primary, secondary};
+
 		/* Inherit component constructors. */
 		using component::component;
+
+		/* Type of this button. */
+		prop<type> type{type::primary};
 
 		/* Initialize a new button. */
 		button()
@@ -19,11 +25,11 @@ namespace ag
 			width = [this] { return font().text_width(text()) + padding().left + padding().right + 2.0f * border().thickness; };
 			height = [this] { return font().line_height() + padding().top + padding().bottom + 2.0f * border().thickness; };
 			padding = [] { return get_theme().button_padding(); };
-			border = {1.0f, color{0, 0, 0, 30}};
+			border = [this] { return ag::border(type() == type::primary, {0, 0, 0, 30}); };
 			radius = [] { return get_theme().button_radius(); };
-			bg_color = [] { return get_theme().button_primary_color(); };
+			bg_color = [this] { return pick_color(type() == type::primary); };
+			text_color = [this] { return pick_color(type() == type::secondary); };
 			font = [] { return get_theme().button_font(); };
-			text_color = [] { return get_theme().button_secondary_color(); };
 			text_align = font::alignment::center;
 		}
 
@@ -34,9 +40,13 @@ namespace ag
 		/* Ripple effect. */
 		animations::ripple ripple_{*this};
 
+		/* Pick primary or secondary color. */
+		static inline color pick_color(bool primary)
+		{ return primary ? get_theme().button_primary_color() : get_theme().button_secondary_color(); }
+
 		/* Will be called when the given event was triggered on this component. */
 		void event_triggered(const events::mouse_entered &) override
-		{ bg_color_ = std::move(bg_color); bg_color = bg_color_().darker(30.0); }
+		{ bg_color_ = std::move(bg_color); bg_color = bg_color_().darker(6.0); }
 
 		/* Will be called when the given event was triggered on this component. */
 		void event_triggered(const events::mouse_left &) override
