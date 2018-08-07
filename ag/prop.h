41acdc6be756f1da4d5f9acbd06d188b87ac8388
Adam Hojka <hojka@programmer.net>
2018-08-07 21:18:02 +0200

    Makes it easier to display numbers.

--- ag/prop.h
+++ ag/prop.h
@@ -3,6 +3,7 @@
 #include <functional>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <utility>
 
 namespace ag
@@ -34,6 +35,11 @@ namespace ag
 		/* Create a new value of the underlying type and forward the given arguments to its constructor. */
 		template <typename ... Args>
 		prop(Args && ... args): prop{[v = std::string{std::forward<Args>(args)...}]() -> std::string_view { return v; }}
+		{}
+
+		/* Create a new value from the given number. */
+		template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+		prop(T number): prop{[v = std::to_string(number)]() -> std::string_view { return v; }}
 		{}
 	};
 }
